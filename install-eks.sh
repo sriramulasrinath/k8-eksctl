c561673c6e84a4017706c5ee64624ab35f4ffb3b
srinath sriramula <sriramulasrinath59@gmail.com>
2024-06-26 16:36:05 +0530

    commiting changes

---
+++ install-eks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.0/2024-05-12/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv kubectl /usr/local/bin
